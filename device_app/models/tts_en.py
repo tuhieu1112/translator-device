31bcb3ffebc1247b7a4db45ffe5e1937158e00be
tuhieu1112 <letuhieu203@gmail.com>
2026-01-09 18:49:34 +0700

    Fix audio device config (numeric ALSA device)

--- device_app/models/tts_en.py
+++ device_app/models/tts_en.py
@@ -5,14 +5,10 @@ from device_app.models.piper_tts import PiperTTS
 
 
 class TTSEn(PiperTTS):
-    """
-    English TTS wrapper (Piper backend)
-    """
-
     def __init__(self, config: dict):
-        tts_cfg = config["TTS"]["EN"]
+        cfg = config["TTS"]["EN"]
 
         super().__init__(
-            model_path=tts_cfg["MODEL_PATH"],
-            piper_exe=tts_cfg["PIPER_EXE"],
+            cfg["MODEL_PATH"],
+            cfg["PIPER_EXE"],
         )
--- device_app/models/tts_vi.py
+++ device_app/models/tts_vi.py
@@ -5,14 +5,10 @@ from device_app.models.piper_tts import PiperTTS
 
 
 class TTSVi(PiperTTS):
-    """
-    Vietnamese TTS wrapper (Piper backend)
-    """
-
     def __init__(self, config: dict):
-        tts_cfg = config["TTS"]["VI"]
+        cfg = config["TTS"]["VI"]
 
         super().__init__(
-            model_path=tts_cfg["MODEL_PATH"],
-            piper_exe=tts_cfg["PIPER_EXE"],
+            cfg["MODEL_PATH"],
+            cfg["PIPER_EXE"],
         )
